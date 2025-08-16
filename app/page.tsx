d6cc0e62489b0159d94f6ecead25190dee0b878e
neillerin99 <lerin113@gmail.com>
2025-08-16 23:47:24 +0800

    fix; removed unneccessary code

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className="font-sans flex flex-col min-h-screen">
       <Hero />
-      <div className="h-40 bg-amber-950 text-white text-3xl">i love you</div>
     </div>
   );
 }
