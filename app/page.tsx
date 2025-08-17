c3a051b0efcdc84635a1246f3a0c67188879b60d
neillerin99 <lerin113@gmail.com>
2025-08-17 19:56:34 +0800

    feat: resume button and download

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Hero from "./sections/hero";
+import About from "./sections/about";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <Hero />
-      <p>Hello Wolr</p>
+      {/* <About /> */}
     </div>
   );
 }
---
+++ app/sections/about.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div className="min-h-96 bg-red-500">Hello world</div>;
+}
