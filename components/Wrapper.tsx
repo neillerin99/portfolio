814274f9116325a84290b4769be0bb5089654509
neillerin99 <lerin113@gmail.com>
2025-08-18 20:32:07 +0800

    fix: switched file name to pascal

---
+++ components/Wrapper.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+
+interface WrapperProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Wrapper({ children, className }: WrapperProps) {
+  return (
+    <div className={cn("w-full max-w-7xl mx-auto px-4", className)}>
+      {children}
+    </div>
+  );
+}
