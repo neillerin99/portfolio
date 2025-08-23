2a42650c689c91c0cde2959bded61aac770d61cb
neillerin99 <lerin113@gmail.com>
2025-08-23 23:10:17 +0800

    feat: added slide animation
    -added slide animation to all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Skill from "./sections/Skill";
 import Experience from "./sections/Experience";
 import Project from "./sections/Project";
+import SlideAnimation from "@/components/motion/SlideAnimation";
 
 export default function Home() {
   return (
